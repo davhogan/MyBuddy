30e872574d74d1fe088f800b3043cf1e239af892
davhogan <davhogan@pdx.edu>
2019-08-15 10:48:21 -0700

    Added Dice roll output. Also made it so the user now uses an input to roll the dice for their turn.

--- src/main.rs
+++ src/main.rs
@@ -77,6 +77,7 @@ impl Player {
 
     //Prints out whatever parts of the body the player has rolled
     pub fn print_body(&self) {
+        println!();
         //Draw the head
         if self.head {
             println!("  0 ");
@@ -134,10 +135,16 @@ impl Player {
     pub fn take_turn(&mut self) {
         let mut rng = rand::thread_rng();
         let mut roll: i64 = rng.gen_range(1, 7);
-
+        let mut flag = 1;
+        println!("{}", roll);
         while !self.already_has_part(roll) && !self.winner(){
-            //println!("Roll is{}", roll);
-            roll = rng.gen_range(0, 6);
+            if flag > 0  {
+                flag *= -1;
+            }
+            else {
+                println!("{}", roll);
+            }
+            roll = rng.gen_range(1, 7);
         }
 
     }
@@ -148,8 +155,6 @@ fn main() {
     let mut human: Player = Player::new();
     let mut ai : Player = Player::new();
 
-
-
     println!("Welcome to the game My Buddy.");
     println!("This is a simple game in which a die is rolled.");
     println!("Each value corresponds to a different body part of the Buddy being drawn.");
@@ -157,17 +162,22 @@ fn main() {
     println!("When the die is rolled if the body part hasn't been drawn then it is added to the drawing.");
     println!("The player continues to roll the die until they can't draw a new body part.");
     println!("First player to draw their Buddy wins");
-    println!("The game will automatically play out. \nIt's you versus the AI good luck!");
-     println!("Enter any key to begin game:");
-     let input : String = read!();
+    println!("It's you versus the AI good luck!");
+    println!("Enter any key to begin game:");
+
+     let mut input : String = read!();
 
     //Play the game
     while !human.winner() || !ai.winner(){
-        sleep(time::Duration::from_millis(1000));
+
         if turn > 0 {
             println!("\nYour turn");
-            println!("Your Buddy:");
+
+            println!("Enter any key to roll:");
+            input = read!();
+            println!("You Rolled:");
             human.take_turn();
+            println!("Your Buddy:");
             human.print_body();
             turn *= -1;
             if human.winner() {
@@ -177,8 +187,10 @@ fn main() {
         }
         else {
             println!("\nAi's turn");
-            println!("AI's Buddy:");
+
+            println!("AI Rolled:");
             ai.take_turn();
+            println!("AI's Buddy:");
             ai.print_body();
             turn *= -1;
             if ai.winner() {
@@ -186,6 +198,7 @@ fn main() {
                 return;
             }
         }
+        sleep(time::Duration::from_millis(750));
     }
 
 }
